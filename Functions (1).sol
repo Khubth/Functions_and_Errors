14fb3c20ed379ef522fae04e06c24d9350d52f13
Khubth <143786320+Khubth@users.noreply.github.com>
2024-07-25 12:44:13 +0530

    Update Functions (1).sol

--- Functions (1).sol
+++ Functions (1).sol
@@ -1,36 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SmartContract {
-    uint public amount;
-
-    // Function with require statement
-    function setAmount(uint _amount) public {
-        require(_amount > 5, "Amount must be greater than five");
-        assert(_amount != 25);
-        amount = _amount;
+contract CollegeAttendance {
+    struct Student {
+        string name;
+        uint attendanceCount;
     }
 
-    // Function with revert statement
-    function setAmountWithRevert(uint _amount) public {
-        if (_amount == 1) {
-            revert("Value cannot be One");
-        }
-        amount = _amount;
+    address public owner;
+    uint public requiredAttendance;
+    mapping(address => Student) public students;
+    mapping(address => bool) public isStudent;
+
+    constructor(uint _requiredAttendance) {
+        owner = msg.sender;
+        requiredAttendance = _requiredAttendance;
     }
 
-    // Function with require statement that checks for even number
-    function setEvenValue(uint _amount) public {
-        require(_amount % 2 == 0, "Amount must be even");
-        assert(_amount!= 25);
-        amount = _amount;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
     }
 
-    // Function with revert statement if value is negative
-    function setAmountPositive(uint _amount) public {
-        if (_amount < 0) {
-            revert("Value must be non-negative");
-        }
-        amount = _amount;
+    modifier onlyStudent() {
+        require(isStudent[msg.sender], "Only students can perform this action");
+        _;
+    }
+
+    function addStudent(address _studentAddress, string memory _name) public onlyOwner {
+        require(!isStudent[_studentAddress], "Student already exists");
+        students[_studentAddress] = Student(_name, 0);
+        isStudent[_studentAddress] = true;
+    }
+
+    function markAttendance() public onlyStudent {
+        students[msg.sender].attendanceCount++;
+    }
+
+    function checkEligibility(address _studentAddress) public view returns (bool) {
+        require(isStudent[_studentAddress], "Invalid student");
+        return students[_studentAddress].attendanceCount >= requiredAttendance;
+    }
+
+    function updateRequiredAttendance(uint _newRequiredAttendance) public onlyOwner {
+        require(_newRequiredAttendance > 0, "Required attendance must be greater than zero");
+        requiredAttendance = _newRequiredAttendance;
+    }
+
+    function revertExae() public pure {
+        revert("This is an example of revert");
     }
 }
